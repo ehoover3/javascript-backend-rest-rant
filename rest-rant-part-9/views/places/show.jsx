f88e1ebc33f9e994dd2203020511c2e32b955942
Eric Hoover <ehoover3@yahoo.com>
2022-03-19 00:55:20 -0700

    completed module 8

---
+++ rest-rant-part-9/views/places/show.jsx
@@ -0,0 +1,36 @@
+const React = require("react");
+const Def = require("../default");
+
+function show(data) {
+  return (<Def>
+    <main>
+      <div className="row">
+        <div className="col-sm-6">
+          <img src={data.place.pic} alt={data.place.name} />
+          <h3>
+            Located in {data.place.city}, {data.place.state}
+          </h3>
+        </div>
+        <div className="col-sm-6">
+          ...
+          <h2>
+            Description
+          </h2>
+          <h3>
+            {data.place.showEstablished()}
+          </h3>
+          <h4>
+            Serving {data.place.cuisines}
+          </h4>
+          ...
+      </div>
+      </div>
+    </main>
+  </Def>
+
+  );
+}
+
+module.exports = show;
+
+
